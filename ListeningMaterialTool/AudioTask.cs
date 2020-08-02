e0bc345e3cb71d9e5e0c28f78d037a8a55629e45
Zhanho Shing <zhanhoshing@icloud.com>
2020-08-02 22:34:10 +0800

    Created AudioTaskItemsCollection Class

--- ListeningMaterialTool/AudioTask.cs
+++ ListeningMaterialTool/AudioTask.cs
@@ -1,4 +1,6 @@
-﻿namespace ListeningMaterialTool {
+﻿using System.Collections.Generic;
+
+namespace ListeningMaterialTool {
     
     /// <summary>
     /// AudioTaskItem will be used to replace ListViewItem.
@@ -10,26 +12,26 @@
         /// <summary>
         /// Initializes the AudioTaskItem instance, use this if NOT adding a built-in audio.
         /// </summary>
-        /// <param name="FilePath">The full path of the audio file.</param>
-        /// <param name="MsIn">The position where the audio should be started trimming (in ms).</param>
-        /// <param name="MsOut">The position where the audio should be ended trimming (in ms).</param>
-        public AudioTaskItem(string FilePath, long MsIn, long MsOut) {
+        /// <param name="filePath">The full path of the audio file.</param>
+        /// <param name="msIn">The position where the audio should be started trimming (in ms).</param>
+        /// <param name="msOut">The position where the audio should be ended trimming (in ms).</param>
+        public AudioTaskItem(string filePath, long msIn, long msOut) {
             
         }
 
         /// <summary>
         /// Initializer for adding silence audio.
         /// </summary>
-        /// <param name="Length">Length of the silence audio (in ms).</param>
-        public AudioTaskItem(long Length) {
+        /// <param name="length">Length of the silence audio (in ms).</param>
+        public AudioTaskItem(long length) {
             
         }
 
         /// <summary>
         /// Initializer for adding Greensleeves audio.
         /// </summary>
-        /// <param name="Length">Length of Greensleeves audio (in seconds).</param>
-        public AudioTaskItem(int Length) {
+        /// <param name="length">Length of Greensleeves audio (in seconds).</param>
+        public AudioTaskItem(int length) {
             
         }
 
@@ -47,7 +49,7 @@
         public long MsIn { get; private set; }
         public long MsOut { get; private set; }
         public long Duration { get; private set; }
-
+        
     }
 
     
@@ -56,5 +58,27 @@
     /// </summary>
     public class AudioTaskItemsCollection {
         
+        // Initializers
+
+        /// <summary>
+        /// Default initializer
+        /// </summary>
+        public AudioTaskItemsCollection() {
+            
+        }
+
+        /// <summary>
+        /// Load all AudioTaskItems from a file
+        /// </summary>
+        /// <param name="filename">Path of the file which will be loaded.</param>
+        public AudioTaskItemsCollection(string filename) {
+            
+        }
+        
+        // Properties
+        /// <summary>
+        /// Stores all AudioTaskItems. DO NOT add to this property directly, use AudioTaskItemsCollection.Append().
+        /// </summary>
+        public List<AudioTaskItem> Items { get; private set; }
     }
 }
