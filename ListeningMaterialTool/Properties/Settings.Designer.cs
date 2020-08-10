acbdf0e4c3981679743173ad31fe48fd94cb01f5
Zhanho Shing <zhanhoshing@icloud.com>
2020-08-10 20:40:27 +0800

    Changes

--- ListeningMaterialTool/Properties/Settings.Designer.cs
+++ ListeningMaterialTool/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace ListeningMaterialTool.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.4.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.9.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -73,7 +73,7 @@ namespace ListeningMaterialTool.Properties {
         
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("v1.4.1-r")]
+        [global::System.Configuration.DefaultSettingValueAttribute("v1.5-b")]
         public string App_VersionName {
             get {
                 return ((string)(this["App_VersionName"]));
